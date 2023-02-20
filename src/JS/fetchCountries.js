3b95a5f9f5470a6a09cffd1c53b8255a3a2f65ca
Kirill Voronin <voroninkv09@gmail.com>
2023-02-20 19:46:36 +0200

    add changes to fetchCountries.js

--- src/JS/fetchCountries.js
+++ src/JS/fetchCountries.js
@@ -1,8 +1,9 @@
 function fetchCountries(input) {
   return fetch(
     `https://restcountries.com/v2/name/${input}?fields=name,capital,population,flag,languages`
-  ).then(resolve => {
+  ).then((resolve, reject) => {
     if (resolve.status == 404) {
+      //throw new Error('Oops, there is no country with that name');
       throw new Error('Oops, there is no country with that name');
     }
     return resolve.json();
